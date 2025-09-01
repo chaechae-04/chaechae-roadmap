f67243b79e1f1caecd19a40eced38bb37afbb01b
lee0su <leeys4903@naver.com>
2025-09-01 11:50:16 +0900

    test: test to Card component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,17 @@
+import Card from '../components/common/Card'
+import useNavigate from '../hook/useNavigate'
+
 function MainPage() {
+  const { navigate } = useNavigate()
+  
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1>MainPage</h1>
+      <Card 
+        title="Todo List" 
+        content="Todo List" 
+        onClick={() => navigate('/todo')} 
+      />
     </div>
   )
 }
